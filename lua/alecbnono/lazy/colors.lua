11381e4c76cf0ee8517f81d101beb63c55709543
alecbnono <alecbnono@gmail.com>
2024-12-01 12:31:19 +0800

    Added Settings

--- lua/alecbnono/lazy/colors.lua
+++ lua/alecbnono/lazy/colors.lua
@@ -93,12 +93,13 @@ return {
 		name = "catpuccin",
 		config = function()
 			require("catppuccin").setup({
+
 				flavour = "mocha", -- latte, frappe, macchiato, mocha
 				background = { -- :h background
 					light = "latte",
 					dark = "mocha",
 				},
-				transparent_background = false, -- disables setting the background color.
+				transparent_background = true, -- disables setting the background color.
 				show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
 				term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
 				dim_inactive = {
@@ -140,6 +141,7 @@ return {
 					-- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
 				},
 			})
+			ColorMyPencils()
 		end,
 	},
 }
