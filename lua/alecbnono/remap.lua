c1a7bbfadb5dbbad4a5f3d239eaee71484ace794
Alec Nono <alecbnono@gmail.com>
2025-01-03 20:04:00 +0800

    Revert "Add page centering remap & vim-be-good extension"
    This reverts commit 9a642b7d16def91589eadb20ef75c223e1ff2e93.

--- lua/alecbnono/remap.lua
+++ lua/alecbnono/remap.lua
@@ -1,11 +1,2 @@
 vim.g.mapleader = " "
-
--- Remap for opening netrw
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-
--- Remap for centering page movement
-vim.keymap.set("n", "<C-d>", "<C-d>zz", { noremap = true, silent = true })
-vim.keymap.set("n", "<C-u>", "<C-u>zz", { noremap = true, silent = true })
-
-vim.keymap.set("n", "n", "nzzzv", { noremap = true, silent = true })
-vim.keymap.set("n", "N", "Nzzzv", { noremap = true, silent = true })
